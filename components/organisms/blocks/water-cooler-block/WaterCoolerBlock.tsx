30677fe1e60ffb78ce17f127b8bf121e71bd3dd1
tretiakovv <55498236+Tretiakovv@users.noreply.github.com>
2024-05-08 17:31:15 +0700

    fix: кулер блок

--- components/organisms/blocks/water-cooler-block/WaterCoolerBlock.tsx
+++ components/organisms/blocks/water-cooler-block/WaterCoolerBlock.tsx
@@ -1,27 +1,23 @@
-import WaterCoolerImage from "@/public/images/water-cooler-image.png"
-
-import WaterdropLineIcon from "@/public/icons/waterdrop-line-icon.png"
-import LeafLineIcon from "@/public/icons/leaf-line-icon.png"
-import VirusLineIcon from "@/public/icons/virus-line-icon.png"
-import AtomLineIcon from "@/public/icons/atom-line-icon.png"
+import WaterCoolerImage from "@/public/images/DIO_Water_Cooler.png"
 
 import Image from "next/image";
 import {HeaderDescription} from "@/types/dto/text";
 import {waterCoolerBlockData} from "@/data/waterCoolerBlockData";
 import Text from "@/components/atoms/text/text-base/Text";
 
-const DescrCol = ({header, description, top, left}: {
+const DescriptionColumn = ({header, description, top, left, right}: {
     description: string,
     top: number,
-    left: number
+    left ?: number,
+    right ?: number
 } & HeaderDescription) => {
     return (
         <div
-            style={{top: top, left: left}}
+            style={{top: top, left: left, right : right}}
             className={"absolute w-[350px] flex flex-col gap-[15px]"}
         >
-            <Text text={header} className={"text-[24px] font-semibold"}/>
-            <Text text={description} className={"text-[20px]"}/>
+            <Text text={header} className={"text-[22px] font-semibold"}/>
+            <Text text={description} className={"text-[18px]"}/>
         </div>
     )
 }
@@ -31,45 +27,20 @@ const WaterCoolerBlock = () => {
     return (
         <div className={"hidden relative w-full sm:flex flex-col items-center"}>
 
-            <Image src={WaterdropLineIcon.src} alt={'/'}
-                   className={"absolute object-fill z-10 left-[355px] top-[130px]"}
-                   width={240} height={100} quality={100}
-            />
-
-            <Image src={AtomLineIcon.src} alt={'/'}
-                   className={"absolute object-fill z-10 right-[342px] top-[230px]"}
-                   width={160} height={100} quality={100}
-            />
-
-            <Image src={LeafLineIcon.src} alt={'/'}
-                   className={"absolute h-[110px] object-scale-down z-10 left-[310px] top-[375px]"}
-                   width={200} height={80} quality={100}
-            />
-
-            <Image src={VirusLineIcon.src} alt={'/'}
-                   className={"absolute object-fill z-10 right-[342px] bottom-[92px]"}
-                   width={160} height={100} quality={100}
-            />
-
             {waterCoolerBlockData.map((item, index) => {
-                return <WaterCoolerBlock.DescrCol
-                    top={item.top}
-                    left={item.left}
-                    header={item.header}
-                    description={item.descr}
-                    key={index}
-                />
+                return <WaterCoolerBlock.DescrCol {...item} description={item.descr} key={index}/>
             })}
 
-            <Image src={WaterCoolerImage.src} alt={'/'}
-                   className={"w-[545px] h-[700px] object-fill"} width={500}
-                   height={160} quality={100}
+            <Image
+                src={WaterCoolerImage.src} alt={'Изображение кулера'}
+                className={"-ml-10 w-[600px] h-[700px] object-scale-down"}
+                width={600} height={700} quality={100}
             />
 
         </div>
     )
 }
 
-WaterCoolerBlock.DescrCol = DescrCol
+WaterCoolerBlock.DescrCol = DescriptionColumn
 
 export default WaterCoolerBlock
