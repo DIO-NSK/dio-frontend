836a3f70358714cd6da71ea5b9d7458eaa2d863f
tretiakovv <55498236+Tretiakovv@users.noreply.github.com>
2024-07-07 19:20:56 +0700

    feat: добавить healthycheck

---
+++ app/(customer)/(site)/(inner-pages)/healthycheck/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HealthyCheckPage = () => {
+    return (
+        <main>
+            <h1>Healthy check</h1>
+        </main>
+    );
+};
+
+export default HealthyCheckPage;
