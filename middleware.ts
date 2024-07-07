d340f99104a352ecacd1e6ddd8afb8f2e9ad60a5
tretiakovv <55498236+Tretiakovv@users.noreply.github.com>
2024-07-07 19:32:39 +0700

    fix: middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,12 @@ import {NextResponse} from 'next/server'
 export function middleware(request: NextRequest) {
     const {nextUrl, headers} = request;
     const ip = headers.get('x-forwarded-for') || request.ip;
-    console.log(request.headers);
 
     if (nextUrl.pathname === '/healthycheck') {
         if (ip === process.env.YANDEX_BALANCER_IP) {
             return NextResponse.next();
+        } else {
+            return NextResponse.redirect(new URL('/', request.url));
         }
     }
 }
