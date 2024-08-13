87cc5c22cd8dcdf92c47dbb8eb59bc946d0707e6
Tretiakovv <tretiakovvvvv@gmail.com>
2024-08-13 12:37:20 +0700

    feat: привязать запросы

--- components/organisms/address-block/AddressBlock.api.ts
+++ components/organisms/address-block/AddressBlock.api.ts
@@ -1,28 +1,23 @@
-import axios from "axios";
-
-const COORDS_TO_LOCATION_URL = 'http://suggestions.dadata.ru/suggestions/api/4_1/rs/geolocate/address'
-const LOCATION_TO_COORDS_URL = 'http://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address'
+import {api} from "@/api";
 
 export interface Suggestion {
-    address : string;
-    lat : number;
-    lng : number;
+    address: string;
+    lat: number;
+    lng: number;
 }
 
-const headers = {
-    headers : {
-        'Authorization' : `Token ${process.env.NEXT_PUBLIC_GEO_KEY}`
-    }
-}
-
-export const getLocationByCoords = async (coords : {lat : number, lng : number}) => {
-    return axios.post(COORDS_TO_LOCATION_URL, {lat : coords.lat, lon : coords.lng}, headers)
+export const getLocationByCoords = async (coords: { lat: number, lng: number }) => {
+    return api.get('/geo/suggest/coords', {params: {latitude: coords.lat, longitude: coords.lng}})
         .then(response => response.data.suggestions)
 }
 
-export const getCoordsByLocation = async (location : string) : Promise<Suggestion[]> => {
-    const response = await axios.post(LOCATION_TO_COORDS_URL, {query : location}, headers);
+export const getCoordsByLocation = async (location: string): Promise<Suggestion[]> => {
+    const response = await api.get('/geo/suggest/name', {params: {name: location}});
     const suggestions = response.data.suggestions;
 
-    return suggestions.map((item : any) => ({address : item.value, lat : Number(item.data.geo_lat), lng : Number(item.data.geo_lon)}))
+    return suggestions.map((item: any) => ({
+        address: item.value,
+        lat: Number(item.data.geo_lat),
+        lng: Number(item.data.geo_lon)
+    }))
 }
