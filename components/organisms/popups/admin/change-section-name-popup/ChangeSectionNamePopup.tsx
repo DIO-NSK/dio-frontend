1a3b033a846ba8caf0b9f6ec9392798eb349f44c
Tretiakovv <tretiakovvvvv@gmail.com>
2024-09-30 23:17:49 +0700

    feat: билд

--- components/organisms/popups/admin/change-section-name-popup/ChangeSectionNamePopup.tsx
+++ components/organisms/popups/admin/change-section-name-popup/ChangeSectionNamePopup.tsx
@@ -1,28 +1,29 @@
-import React, {useState} from 'react';
-import {TableItemPopup} from "@/types/props/Popup";
+import React, { useState } from 'react';
+import { TableItemPopup } from "@/types/props/Popup";
 import PopupWrapper from "@/components/wrappers/popup-wrapper/PopupWrapper";
 import Text from "@/components/atoms/text/text-base/Text";
 import TextInput from "@/components/atoms/inputs/text-input/TextInput";
 import Button from "@/components/atoms/buttons/button/Button";
-import {useUnit} from "effector-react";
-import {editSectionEvent, onCloseSectionToEditEvent} from "@/app/admin/catalog/model";
+import { useUnit } from "effector-react";
+import { editSectionEvent, onCloseSectionToEditEvent } from "@/app/admin/catalog/model";
 
 const ChangeSectionNamePopup = (props: TableItemPopup<string[]>) => {
 
     const [editSection, onCloseSectionToEdit] = useUnit([editSectionEvent, onCloseSectionToEditEvent])
 
     const [sectionName, setSectionName] = useState<string>("")
+
     const handleSubmit = () => {
-        editSection({sectionId: props.tableRow.id, newName: sectionName})
+        // editSection({sectionId: props.tableRow.id, newName: sectionName as any})
         onCloseSectionToEdit()
     }
 
     return (
         <PopupWrapper placement={"center"} {...props}>
             <div className={"w-[400px] flex flex-col gap-5"}>
-                <Text text={"Изменить название"} className={"text-[20px] font-medium"}/>
+                <Text text={"Изменить название"} className={"text-[20px] font-medium"} />
                 <Text text={"Будьте осторожны, новое название применится ко всем разделам сайта"}
-                      className={"text-text-gray"}/>
+                    className={"text-text-gray"} />
                 <TextInput
                     labelText={"Название раздела"}
                     placeholder={props.tableRow.item[0]}
