2450d47b1f695071b906ba6148adc9925c2e6817
Konstantinov Nikita <n.konstantinov@g.nsu.ru>
2024-11-06 10:30:52 +0700

    feat: dioshop

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
-export const BASE_URL: string = "https://diowater.ru/api"
+export const BASE_URL: string = "https://dioshop.ru/api"
 
 export const api = axios.create({
     baseURL: BASE_URL,
